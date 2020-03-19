046c358f32ba98e8026cd363e7881d77b1985709
SMony-L <l.sovanmony@gmail.com>
2020-03-19 14:08:32 -0400

    adding projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,8 +12,17 @@ class Projects extends Component {
       <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="projects">
         <div className="w-100">
           <h2 className="mb-5">Projects</h2>
-          <p>{this.projects.paragraphOne}</p>
-          <p className="mb-0">{this.projects.paragraphTwo}</p>
+          {
+            this.projects.map((proj, index) => (
+              <div key={index} className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div className="resume-content">
+                  <h3 className="mb-0">{proj.title}</h3>
+                  <div className="subheading mb-3">{proj.projectLang}</div>
+                  <p>{proj.aboutProject}</p>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </section>
     );
